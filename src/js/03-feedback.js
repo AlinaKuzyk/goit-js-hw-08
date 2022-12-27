2e9e7bde500d57d24f946dacf0b704fb1d0f0e41
Alina_Kuzyk <dr.alinakuzyk@gmail.com>
2022-12-27 18:52:24 +0100

    update 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,32 +4,30 @@ const formEl = document.querySelector('.feedback-form');
 const emailEl = document.querySelector('input');
 const messagelEl = document.querySelector('textarea');
 const LOCALE_STORAGE = 'feedback-form-state';
-
-const data = JSON.parse(localStorage.getItem(LOCALE_STORAGE)) || {
-  email: '',
-  message: '',
-};
-
-savedMessage();
-
-const handleSubmitForm = event => {
-  event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem(LOCALE_STORAGE);
-};
+let data = {};
 
 const handleChangeForm = elem => {
   data[elem.target.name] = elem.target.value;
   localStorage.setItem(LOCALE_STORAGE, JSON.stringify(data));
 };
 
+const handleSubmitForm = event => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALE_STORAGE);
+  data = {};
+};
+
+savedMessage();
+
 function savedMessage() {
+  data = JSON.parse(localStorage.getItem(LOCALE_STORAGE)) || {};
   const formData = data;
   const email = emailEl;
   const message = messagelEl;
   if (formData) {
-    email.value = data.email;
-    message.value = data.message;
+    email.value = data.email || '';
+    message.value = data.message || '';
   }
 }
 
